76492276c99cf0adf6f139dd8cc49f546e793056
Anujin <anujin@chimege.com>
2026-04-27 19:34:38 +0800

    added shadcn ui components and setup frontend project

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,128 +1,20 @@
-import vuetify, { transformAssetUrls } from "vite-plugin-vuetify"
-
-const isDev = process.env.NODE_ENV !== "production"
-
-const domain = process.env.DOMAIN
-
-console.log("Server type:", process.env.SERVER_TYPE)
-
-const nitro =
-  process.env.SERVER_TYPE != "local"
-    ? {
-        routeRules: {
-          "pub/**": { proxy: "http://localhost:3000/api/**" },
-        },
-      }
-    : {
-        routeRules: {
-          "/api/**": { proxy: "http://localhost:4000/api/**" },
-          "/pub/**": { proxy: "http://localhost:4000/pub/**" },
-        },
-        devProxy: {
-          "/api": {
-            target: "http://localhost:4000/api/",
-            changeOrigin: true,
-            ws: true,
-          },
-          "/pub": {
-            target: "http://localhost:4000/pub/",
-            changeOrigin: true,
-            ws: true,
-          },
-        },
-      }
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
-  build: {
-    transpile: ["vuetify"],
+  modules: ["shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: "@/components/ui",
   },
-
-  plugins: [
-    "~/plugins/toast.ts",
-    "~/plugins/formatNumberWithKAndM.js",
-    "~/plugins/vue-zoomer.js",
-    "~/plugins/mitt.ts",
-  ],
-
-  modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook("vite:extendConfig", (config) => {
-        // @ts-expect-error
-        config.plugins.push(
-          vuetify({
-            autoImport: true,
-            styles: {
-              configFile: "./assets/settings.scss",
-            },
-          }),
-        )
-      })
-    },
-  ],
-
-  ssr: false,
-
-  sourcemap: {
-    server: false,
-    client: false,
-  },
-
-  runtimeConfig: {
-    public: {
-      dev: isDev,
-      domain: domain,
-    },
-  },
-
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
-    },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          silenceDeprecations: ["legacy-js-api"],
-        },
-        sass: {
-          silenceDeprecations: ["legacy-js-api"],
-        },
-      },
-    },
-  },
-
-  nitro,
-
-  app: {
-    head: {
-      title: "SkillRecruit",
-      titleTemplate: "%s",
-      link: [
-        { rel: "stylesheet", href: "/feather/style.css" },
-        { rel: "icon", type: "image/x-icon", href: "/icons/icon.svg" },
-        { rel: "logo-brand", sizes: "180x180", href: "/icons/icon.svg" },
-        {
-          rel: "icon",
-          type: "image/svg",
-          sizes: "32x32",
-          href: "/icons/icon.svg",
-        },
-        {
-          rel: "icon",
-          type: "image/svg",
-          sizes: "16x16",
-          href: "/icons/icon.svg",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap",
-        },
-      ],
-    },
-  },
-
-  compatibilityDate: "2024-12-18",
-})
+});
--- frontend/plugins/mitt.ts
+++
@@ -1,9 +0,0 @@
-import mitt from "mitt"
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const emitter = mitt()
-
-  nuxtApp.provide("emitEvent", emitter.emit) // Emit events
-  nuxtApp.provide("listen", emitter.on) // Listen for events
-  nuxtApp.provide("clear", emitter.off) // Clear listeners
-})
